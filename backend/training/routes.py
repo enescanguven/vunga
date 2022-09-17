dcd8fbea09076aa78aa871429b6fdc4e1eb3e8d3
enescanguven <enescanguven@gmail.com>
2022-09-17 18:36:17 +0300

    conflict merge

--- backend/training/routes.py
+++ backend/training/routes.py
@@ -1,24 +1,19 @@
-from contextlib import closing
-from signal import SIGKILL
-import socket
+from bson.json_util import dumps
+import json
 from flask import Blueprint, current_app, request
-from celery import Celery
-import os
-from subprocess import Popen, PIPE
-from os import kill
+
 
 training_bp = Blueprint('training_bp', __name__, template_folder='templates')
 
-@training_bp.route('/training', methods=['POST'])
-def train_model():
-    train_config = request.get_json()
-    
+
+@training_bp.route('/training', methods=['GET'])
+def get_all_trainings():
     try:
-        job_id = str(current_app.config["celery"].send_task(
-            "cyberdroid_tasks.train", args=[train_config]))
-
-        return {"port": "Model"}, 200
-
+        db = current_app.config["db_conn"]
+        trainings = db["trainings"]
+        cursor = list(trainings.find({}))
+        json_data = json.loads(dumps(cursor))
+        return {"trainings": json_data}
     except Exception as e:
         print(str(e))
         return {
@@ -26,5 +21,10 @@ def train_model():
             "reason": str(e),
         }, 500
 
-# celery = current_app.config['celery']
 
+@training_bp.route('/training/start', methods=['POST'])
+def start_training():
+    db = current_app.config["db_conn"]
+    trainings = db["trainings"]
+
+    return 'Start Training'
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,10 +5,12 @@ const InfluencerPage = React.lazy(() => import('./views/Influencer/InfluencerPag
 const StockPage = React.lazy(() => import('./views/Stock/StockPage'))
 const metricPage = React.lazy(() => import('./views/Metrics/metricPage'))
 const CameraPage = React.lazy(() => import('./views/Camera/CameraPage'))
+const TrainingsPage = React.lazy(() => import('./views/Trainings/TrainingsPage'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/camera', name: 'Camera', element: CameraPage },
+  { path: '/trainings', name: 'Trainings', element: TrainingsPage },
   { path: '/admin', name: 'Admin', element: AdminPage },
   { path: '/stock', name: 'Stock', element: StockPage },
   { path: '/influencer', name: 'Influencer', element: InfluencerPage },
